c9e98db9bfed0fc0dc1e80f3e4a454cd84b352cd
WaltherJMG <wmatute5454@utm.edu.ec>
2026-01-09 01:49:00 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000
 
 const withelist = [
     'http://localhost:5173',
-    'https://sistemainventario-backend.onrender.com'
+    'sistema-inventario-frontend-beige.vercel.app'
 ]
 
 app.use(morgan('dev'))
