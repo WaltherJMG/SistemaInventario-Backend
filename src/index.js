b8fda2610f6568d0abd49d744aac9ff6fc13ad1d
WaltherJMG <wmatute5454@utm.edu.ec>
2026-01-09 02:04:21 -0500

    deploy 3

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000
 
 const withelist = [
     'http://localhost:5173',
-    'sistema-inventario-frontend-beige.vercel.app',
+    'https://sistema-inventario-frontend-beige.vercel.app',
     'https://sistemainventario-backend.onrender.com'
 ]
 
